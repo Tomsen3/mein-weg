f8d0309b470422e12c16c9539cde24aa7d2f2ec9
Tomsen3 <tom.jansen@singende-krankenhaeuser.de>
2026-04-24 05:51:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const SB_URL     = 'https://cnlgpviurgpxcrjtfxqi.supabase.co';
 const SB_ANON    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubGdwdml1cmdweGNyanRmeHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NjExMjQsImV4cCI6MjA4ODMzNzEyNH0.FQ842Ete0xJ1MgCM0aBejDVkBL15-OaCGuN_0Cu80Og';
 
 // VERSION
-const APP_VERSION        = '1.9';
+const APP_VERSION        = '1.10';
 const VERSION_URL        = 'https://raw.githubusercontent.com/tomsen3/mein-weg/main/version.json';
 const UPDATE_INTERVAL_MS = 6 * 60 * 60 * 1000; // 6 Stunden
 
@@ -18,7 +18,13 @@ const UPDATE_INTERVAL_MS = 6 * 60 * 60 * 1000; // 6 Stunden
 function getUserId() {
   let uid = localStorage.getItem('meinweg_uid');
   if (!uid) {
-    uid = crypto.randomUUID();
+    uid = (window.crypto && typeof window.crypto.randomUUID === 'function')
+      ? window.crypto.randomUUID()
+      : 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+          const r = Math.random() * 16 | 0;
+          const v = c === 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
     localStorage.setItem('meinweg_uid', uid);
   }
   return uid;
