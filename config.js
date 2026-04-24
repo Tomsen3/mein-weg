09cc10307ed1cb2bc5ba5bbc0e3bd650a83d2ab8
Tomsen3 <tom.jansen@singende-krankenhaeuser.de>
2026-04-24 07:02:54 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const SB_URL     = 'https://cnlgpviurgpxcrjtfxqi.supabase.co';
 const SB_ANON    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubGdwdml1cmdweGNyanRmeHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NjExMjQsImV4cCI6MjA4ODMzNzEyNH0.FQ842Ete0xJ1MgCM0aBejDVkBL15-OaCGuN_0Cu80Og';
 
 // VERSION
-const APP_VERSION        = '1.17';
+const APP_VERSION        = '1.18';
 const VERSION_URL        = 'https://raw.githubusercontent.com/tomsen3/mein-weg/main/version.json';
 const UPDATE_INTERVAL_MS = 6 * 60 * 60 * 1000; // 6 Stunden
 
